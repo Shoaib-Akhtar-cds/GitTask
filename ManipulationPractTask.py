050004aa2247428943ba315908e466fb9ad55312
Shoaib-Akhtar-cds <shoaibcds1@gmail.com>
2024-03-28 01:43:54 +0000

    Further Enhancement Issue-1

--- ManipulationPractTask.py
+++ ManipulationPractTask.py
@@ -5,8 +5,8 @@ s_last_char =s_user_text[len(s_user_text)-1] #getting last character of sentence
 s_text_last_char_replaced=  s_user_text.replace(s_last_char,"@") #replacing last character with @
 print(s_text_last_char_replaced)
 
-sLast3CharsReversed = sUserText[len(sUserText):len(sUserText)-4:-1] #Getting last three characters in reversed order
-print(sLast3CharsReversed)
+s_last3_chars_reversed = s_user_text[len(s_user_text):len(s_user_text)-4:-1] #Getting last three characters in reversed order
+print(s_last3_chars_reversed)
 
-sRequiredWord = sUserText[0:3] + sUserText[len(sUserText)-2:len(sUserText)]
-print(sRequiredWord)
+s_required_word = s_user_text[0:3] + s_user_text[len(s_user_text)-2:len(s_user_text)]
+print(s_required_word)
