ec3e4b5afe25eaed3eb1378dee39c4bbc6ac13d1
Shoaib-Akhtar-cds <shoaibcds1@gmail.com>
2024-03-28 01:38:08 +0000

    Changed variable names to snake case as asked in Issue-1 Enhancements.

--- ManipulationPractTask.py
+++ ManipulationPractTask.py
@@ -1,9 +1,9 @@
-sUserText = ""
-sUserText = input("Please Enter Some Text : ")
-print(f"The Length Of Text is {len(sUserText)}")
-sLastChar =sUserText[len(sUserText)-1] #getting last character of sentence
-sTextLastCharRplaced=  sUserText.replace(sLastChar,"@") #replacing last character with @
-print(sTextLastCharRplaced)
+s_user_text = ""
+s_user_text = input("Please Enter Some Text : ")
+print(f"The Length Of Text is {len(s_user_text)}")
+s_last_char =s_user_text[len(s_user_text)-1] #getting last character of sentence
+s_text_last_char_replaced=  s_user_text.replace(s_last_char,"@") #replacing last character with @
+print(s_text_last_char_replaced)
 
 sLast3CharsReversed = sUserText[len(sUserText):len(sUserText)-4:-1] #Getting last three characters in reversed order
 print(sLast3CharsReversed)
